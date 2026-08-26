mightyiam/infra

commit c3f66a0d6bfa9d18ac490be29ff1f8e1fba6c123

xresources: add berber to maintainers

modules/xresources/meta.nix
@@ -1,5 +1,6 @@
+{ lib, ... }:
 {
   name = "Xresources file";
   homepage = "https://www.x.org/wiki";
-  maintainers = [ ];
+  maintainers = [ lib.maintainers.berber ];
 }

stylix/maintainers.nix
@@ -7,6 +7,12 @@
 # [1]: https://github.com/NixOS/nixpkgs/blob/1da63e6cc622a0cb6fd5b86d49923e4eb1e33b70/maintainers/maintainer-list.nix
 {
   # keep-sorted start case=no numeric=no block=yes
+  berber = {
+    email = "git@zmberber.com";
+    name = "Zeremonienmeister Berber Aab";
+    github = "zmberber";
+    githubId = 29400525;
+  };
   butzist = {
     email = "adam@szalkowski.de";
     name = "Adam M. Szalkowski";

generated/all-maintainers.nix
@@ -239,4 +239,10 @@
     githubId = 41439633;
     name = "Vidhan Bhatt";
   };
+  zmberber = {
+    email = "git@zmberber.com";
+    github = "zmberber";
+    githubId = 29400525;
+    name = "Zeremonienmeister Berber Aab";
+  };
 }

AndreBClark/stylix

commit 2769b926ff356f3dbe41ac8888da72c92f1a53c7

xresources/meta: rename berber maintainer
Fixes: c3f66a0d6bfa ("xresources: add berber to maintainers")
Link: https://github.com/nix-community/stylix/pull/2420

modules/xresources/meta.nix
@@ -1,5 +1,5 @@
 { lib, ... }: {
   name = "Xresources file";
   homepage = "https://www.x.org";
-  maintainers = [ lib.maintainers.berber ];
+  maintainers = [ lib.maintainers.zmberber ];
 }

stylix/maintainers.nix
@@ -13,12 +13,6 @@
     github = "andrebclark";
     githubId = 48167848;
   };
-  berber = {
-    email = "git@zmberber.com";
-    name = "Zeremonienmeister Berber Aab";
-    github = "zmberber";
-    githubId = 29400525;
-  };
   butzist = {
     email = "adam@szalkowski.de";
     name = "Adam M. Szalkowski";
@@ -111,6 +105,12 @@
     name = "Vidhan Bhatt";
     github = "vidhanio";
     githubId = 41439633;
+  };
+  zmberber = {
+    email = "git@zmberber.com";
+    name = "Zeremonienmeister Berber Aab";
+    github = "zmberber";
+    githubId = 29400525;
   };
   # keep-sorted end
 }